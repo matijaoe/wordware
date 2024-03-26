66d95877c5847c2ecdf6e86e9d79eb3a53bfc64b
Matija Osrečki <github@matijao.com>
2024-03-26 12:49:12 +0100

    feat: work on wordlist slug page layout

--- layouts/half.vue
+++ layouts/half.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="grid grid-cols-2 gap-8">
-    <div class="p-4 sm:p-8 pr-0">
+  <div class="grid xl:grid-cols-2 overflow-auto gap-4 h-screen">
+    <div class="p-4 sm:p-8 pr-0 overflow-y-auto">
       <Header />
 
       <main class="mt-6">
         <slot />
       </main>
     </div>
+
     <slot name="right" />
   </div>
 </template>
