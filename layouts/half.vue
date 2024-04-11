1d1295132585ff32f0ecfa17df62ffca3e3a28a9
Matija Osrečki <github@matijao.com>
2024-04-11 09:38:46 +0200

    chore: update deps

--- layouts/half.vue
+++ layouts/half.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid xl:grid-cols-2 overflow-auto gap-4 h-screen">
-    <div class="p-4 sm:py-5 sm:px-8 pr-0 overflow-y-auto">
+    <div class="p-4 sm:py-5a sm:px-8 pr-0 overflow-y-auto">
       <Header class="mb-4" />
 
       <main>
